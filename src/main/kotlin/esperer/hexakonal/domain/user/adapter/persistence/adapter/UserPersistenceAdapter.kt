1807c8c39c836f6130706cf40e96c61dcfa93746
aesperer <piyrw9754@gmail.com>
2023-03-09 17:31:03 +0900

    :art: user persistence adpater

--- src/main/kotlin/esperer/hexakonal/domain/user/adapter/persistence/adapter/UserPersistenceAdapter.kt
+++ src/main/kotlin/esperer/hexakonal/domain/user/adapter/persistence/adapter/UserPersistenceAdapter.kt
@@ -1,11 +1,28 @@
 package esperer.hexakonal.domain.user.adapter.persistence.adapter
 
+import esperer.hexakonal.domain.user.User
+import esperer.hexakonal.domain.user.adapter.persistence.converter.UserConverter
 import esperer.hexakonal.domain.user.adapter.persistence.repository.UserRepository
+import esperer.hexakonal.domain.user.application.port.UserPort
 import esperer.hexakonal.global.annotation.adapter.TransactionalAdapter
+import org.springframework.data.repository.findByIdOrNull
+import java.util.*
 
 @TransactionalAdapter
 class UserPersistenceAdapter(
     private val userRepository: UserRepository,
     private val userConverter: UserConverter
-) {
+): UserPort {
+    override fun saveUser(user: User, password: String): User =
+        userConverter.toDomain(userRepository.save(userConverter.toEntity(user, password)))
+
+    override fun existsUserByEmail(email: String): Boolean =
+        userRepository.existsByEmail(email)
+
+    override fun queryUserByEmail(email: String): User? =
+        userRepository.findByEmail(email)?.let { userConverter.toDomain(it) }
+
+    override fun queryUserById(id: UUID): User? =
+        userRepository.findByIdOrNull(id)?.let { userConverter.toDomain(it) }
+
 }
--- src/main/kotlin/esperer/hexakonal/domain/user/adapter/persistence/repository/UserRepository.kt
+++ src/main/kotlin/esperer/hexakonal/domain/user/adapter/persistence/repository/UserRepository.kt
@@ -1,8 +1,11 @@
 package esperer.hexakonal.domain.user.adapter.persistence.repository
 
+import esperer.hexakonal.domain.user.User
 import esperer.hexakonal.domain.user.adapter.persistence.entity.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface UserRepository : JpaRepository<UserEntity, UUID> {
+    fun existsByEmail(email: String): Boolean
+    fun findByEmail(email: String): UserEntity?
 }
