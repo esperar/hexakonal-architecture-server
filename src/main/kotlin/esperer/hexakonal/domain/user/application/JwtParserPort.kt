fe265221cb30f5e0e1274372ba35370b6b14bfc2
aesperer <piyrw9754@gmail.com>
2023-03-09 11:40:07 +0900

    :sparkles: jwt parser adapter implements

--- src/main/kotlin/esperer/hexakonal/domain/user/application/JwtParserPort.kt
+++ src/main/kotlin/esperer/hexakonal/domain/user/application/JwtParserPort.kt
@@ -1,11 +1,12 @@
 package esperer.hexakonal.domain.user.application
 
+import org.springframework.security.core.Authentication
 import javax.servlet.http.HttpServletRequest
 
 interface JwtParserPort {
 
     fun parseAccessToken(request: HttpServletRequest): String?
     fun parseRefreshToken(refreshToken: String): String?
-    fun authentication(accessToken: String): String?
+    fun authentication(accessToken: String): Authentication
     fun isRefreshTokenExpired(refreshToken: String): Boolean
 }
