061221b86889f24c097b174a32b8331a26e2d390
aesperer <piyrw9754@gmail.com>
2023-03-09 17:36:25 +0900

    :sparkles: signup user auth adapter

---
+++ src/main/kotlin/esperer/hexakonal/domain/user/adapter/presentation/UserAuthWebAdapter.kt
@@ -0,0 +1,23 @@
+package esperer.hexakonal.domain.user.adapter.presentation
+
+import esperer.hexakonal.domain.user.adapter.presentation.data.request.SignUpRequest
+import esperer.hexakonal.domain.user.application.usecase.SignUpUseCase
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/api/v1/auth")
+class UserAuthWebAdapter(
+    private val signUpUseCase: SignUpUseCase
+) {
+
+    @PostMapping("/signup")
+    fun signUp(@RequestBody request: SignUpRequest): ResponseEntity<Void> =
+        signUpUseCase.execute(request)
+            .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+}
