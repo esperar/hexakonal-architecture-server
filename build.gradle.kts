efffe289228f79182571c87c5a6e718a9dcb06c5
aesperer <piyrw9754@gmail.com>
2023-03-09 10:59:59 +0900

    :arrow_up: jwt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,12 @@ dependencies {
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/main/kotlin/esperer/hexakonal/global/security/SecurityConfig.kt
@@ -0,0 +1,9 @@
+package esperer.hexakonal.global.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+}
