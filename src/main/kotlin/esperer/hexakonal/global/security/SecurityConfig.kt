774294267487415f62d6ebc19124b9bbc0d7a855
aesperer <piyrw9754@gmail.com>
2023-03-09 12:28:10 +0900

    :sparkles: Security Configuration

--- src/main/kotlin/esperer/hexakonal/global/security/SecurityConfig.kt
+++ src/main/kotlin/esperer/hexakonal/global/security/SecurityConfig.kt
@@ -1,9 +1,56 @@
 package esperer.hexakonal.global.security
 
+import esperer.hexakonal.domain.user.application.JwtParserPort
+import esperer.hexakonal.global.security.handler.CustomAccessDenied
+import esperer.hexakonal.global.security.handler.CustomAuthenticationEntryPoint
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import javax.servlet.FilterConfig
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtParserPort: JwtParserPort
+) {
+
+    @Bean
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain =
+        http
+            .cors()
+            .and()
+            .csrf().disable()
+            .httpBasic().disable()
+
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+
+            .authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/api/v1/auth/signup").permitAll()
+            .antMatchers(HttpMethod.POST, "/api/v1/auth/signin").permitAll()
+            .antMatchers(HttpMethod.PATCH, "/api/v1/auth/reissue").permitAll()
+            .anyRequest().authenticated()
+            .and()
+
+            .exceptionHandling()
+            .authenticationEntryPoint(CustomAuthenticationEntryPoint())
+            .accessDeniedHandler(CustomAccessDenied())
+            .and()
+
+            //.apply(FilterConfig(jwtParserPort))
+            //.and()
+            .build()
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+
+
+
 }
