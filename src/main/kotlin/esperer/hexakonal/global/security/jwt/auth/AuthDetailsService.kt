d8b06778ca8697aa25db87c5b9a020063f54a6bc
aesperer <piyrw9754@gmail.com>
2023-03-09 11:24:53 +0900

    :sparkles: AuthDetailsService

---
+++ src/main/kotlin/esperer/hexakonal/global/security/jwt/auth/AuthDetailsService.kt
@@ -0,0 +1,20 @@
+package esperer.hexakonal.global.security.jwt.auth
+
+import esperer.hexakonal.domain.user.adapter.persistence.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+@Transactional(readOnly = true, rollbackFor = [Exception::class])
+class AuthDetailsService(
+    private val userRepository: UserRepository
+): UserDetailsService {
+
+    override fun loadUserByUsername(username: String?): UserDetails =
+        AuthDetails(userRepository.findByIdOrNull(UUID.fromString(username)) ?: throw RuntimeException())
+
+}
