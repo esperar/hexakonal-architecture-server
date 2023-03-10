fe76a3501c8cc89fdb4f5e2c91737f9e9c6747a1
aesperer <piyrw9754@gmail.com>
2023-03-10 12:19:01 +0900

    :sparkles: presentation login api

--- src/main/kotlin/esperer/hexakonal/domain/user/adapter/presentation/UserAuthWebAdapter.kt
+++ src/main/kotlin/esperer/hexakonal/domain/user/adapter/presentation/UserAuthWebAdapter.kt
@@ -1,6 +1,9 @@
 package esperer.hexakonal.domain.user.adapter.presentation
 
+import esperer.hexakonal.domain.user.adapter.presentation.data.request.SignInRequest
 import esperer.hexakonal.domain.user.adapter.presentation.data.request.SignUpRequest
+import esperer.hexakonal.domain.user.adapter.presentation.data.response.TokenResponse
+import esperer.hexakonal.domain.user.application.usecase.SignInUseCase
 import esperer.hexakonal.domain.user.application.usecase.SignUpUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -12,11 +15,17 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/auth")
 class UserAuthWebAdapter(
-    private val signUpUseCase: SignUpUseCase
+    private val signUpUseCase: SignUpUseCase,
+    private val signInUseCase: SignInUseCase
 ) {
 
     @PostMapping("/signup")
     fun signUp(@RequestBody request: SignUpRequest): ResponseEntity<Void> =
         signUpUseCase.execute(request)
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @PostMapping("/login")
+    fun signIn(@RequestBody request: SignInRequest): ResponseEntity<TokenResponse> =
+        signInUseCase.execute(request)
+            .let { ResponseEntity.ok(it) }
 }
