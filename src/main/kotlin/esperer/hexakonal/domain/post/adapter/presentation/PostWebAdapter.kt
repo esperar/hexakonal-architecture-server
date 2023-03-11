db3d3f9534ed109cb4c792bd4f238329670cb6b6
aesperer <piyrw9754@gmail.com>
2023-03-12 01:17:03 +0900

    :sparkles: post svae

--- src/main/kotlin/esperer/hexakonal/domain/post/adapter/presentation/PostWebAdapter.kt
+++ src/main/kotlin/esperer/hexakonal/domain/post/adapter/presentation/PostWebAdapter.kt
@@ -1,10 +1,23 @@
 package esperer.hexakonal.domain.post.adapter.presentation
 
+import esperer.hexakonal.domain.post.adapter.presentation.data.request.CreatePostRequest
+import esperer.hexakonal.domain.post.application.usecase.SavePostUseCase
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/post")
-class PostWebAdapter {
+class PostWebAdapter(
+    private val savePostUseCase: SavePostUseCase
+) {
+
+    @PostMapping
+    fun savePost(@RequestBody request: CreatePostRequest): ResponseEntity<Void> =
+        savePostUseCase.execute(request)
+            .let { ResponseEntity.status(HttpStatus.CREATED).build()}
 
 }
