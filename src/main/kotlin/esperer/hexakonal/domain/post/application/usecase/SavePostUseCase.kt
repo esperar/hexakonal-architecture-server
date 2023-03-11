97b7256b2822b204e3e298e657f4590aa2d49aea
aesperer <piyrw9754@gmail.com>
2023-03-12 01:15:38 +0900

    :sparkles: save port usercase

--- src/main/kotlin/esperer/hexakonal/domain/post/application/usecase/SavePostUseCase.kt
+++ src/main/kotlin/esperer/hexakonal/domain/post/application/usecase/SavePostUseCase.kt
@@ -1,10 +1,30 @@
 package esperer.hexakonal.domain.post.application.usecase
 
+import esperer.hexakonal.domain.post.Post
+import esperer.hexakonal.domain.post.adapter.presentation.data.request.CreatePostRequest
 import esperer.hexakonal.domain.post.application.spi.CommandPostPort
+import esperer.hexakonal.domain.user.application.spi.UserSecurityPort
 import esperer.hexakonal.global.annotation.usecase.TransactionalUseCase
+import java.util.*
+import kotlin.contracts.contract
 
 @TransactionalUseCase
 class SavePostUseCase(
     private val commandPostPort: CommandPostPort,
+    private val userSecurityPort: UserSecurityPort
 ) {
+
+    fun execute(request: CreatePostRequest): UUID {
+        val post = request.let {
+            Post(
+                id = UUID.randomUUID(),
+                title = it.title,
+                content = it.content,
+                userId = userSecurityPort.getCurrentUserId(),
+                tag = it.tag
+
+            )
+        }
+        return commandPostPort.savePost(post).id
+    }
 }
--- src/main/kotlin/esperer/hexakonal/domain/user/adapter/security/UserSecurityAdapter.kt
+++ src/main/kotlin/esperer/hexakonal/domain/user/adapter/security/UserSecurityAdapter.kt
@@ -1,9 +1,11 @@
 package esperer.hexakonal.domain.user.adapter.security
 
 import esperer.hexakonal.domain.user.application.spi.UserSecurityPort
+import esperer.hexakonal.global.annotation.adapter.ReadOnlyAdapter
 import org.springframework.security.core.context.SecurityContextHolder
 import java.util.*
 
+@ReadOnlyAdapter
 class UserSecurityAdapter: UserSecurityPort {
 
     override fun getCurrentUserId(): UUID =
