4c70476782123e5b8f9f5245e593b7f9eea46f5f
aesperer <piyrw9754@gmail.com>
2023-03-12 00:35:06 +0900

    :sparkles: post web adapter

---
+++ src/main/kotlin/esperer/hexakonal/domain/post/adapter/presentation/PostWebAdapter.kt
@@ -0,0 +1,10 @@
+package esperer.hexakonal.domain.post.adapter.presentation
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/post")
+class PostWebAdapter {
+
+}
